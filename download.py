73e312be33388301f5776098db852083635869ff
Aiden Schulman <aidenhschulman@gmail.com>
2023-01-21 22:42:46 -0500

    Added download label and button, widgets to screen

--- download.py
+++ download.py
@@ -24,5 +24,20 @@ canvas.pack()
 link_field = Entry(screen, width=50)
 link_label = Label(screen, text="Enter YouTube Link: ")
 
+# Select Path for saving file
+path_label = Label(screen, text="Select File Path for Video:")
+select_btn = Button(screen, text="Select", command=select_path)
+
+canvas.create_window(250, 130, window=path_label)
+canvas.create_window(250, 180, window=select_btn)
+
+# Add widgets to window
+canvas.create_window(250, 20, window=link_label)
+canvas.create_window(250, 70, window=link_field)
+
+# Download buttons
+download_btn = Button(screen, text="Download Video", command=download_file)
+canvas.create_window(250, 240, window=download_btn)
+
 # App loop
 screen.mainloop()
