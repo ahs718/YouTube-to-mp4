ea3483d5601f3f582f6a6d6d42606a628219e321
Aiden Schulman <aidenhschulman@gmail.com>
2023-01-21 22:41:43 -0500

    Added app frame

--- download.py
+++ download.py
@@ -4,3 +4,22 @@ from moviepy import *
 from moviepy.editor import VideoFileClip
 from pytube import YouTube
 import shutil
+
+
+# Functions
+def select_path():
+    # Prompts user to select path from the file explorer
+    path = filedialog.askdirectory()
+    path_label.configure(text=path)
+
+
+
+# App frame
+screen = Tk()
+title = screen.title("Youtube to mp4")
+canvas = Canvas(screen, width=480, height=300)
+canvas.pack()
+
+
+# App loop
+screen.mainloop()
